5c5dd49ca0371ce0b2743d25623679657b4a1e49
hashem98 <hashemsmadi98@gmail.com>
2022-02-15 06:29:43 +0200

    still not complite

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,17 @@
+import "./Header.css"
+import { Link } from 'react-router-dom';
 function Header(props) {
     return (
 
         <>
-          <h1 className="header">Travel Destination</h1>
+        <header>         
+        <nav>
+                <Link to='/'>  Home </Link>
+            </nav>
+             <h1 className="header">Travel Destination</h1>
+         
+            </header>
+
         </>
 
     );
